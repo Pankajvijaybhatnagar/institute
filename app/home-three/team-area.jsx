d71a61a5e09c67181e4f482f1ee1bf5f08648339
ankitjnvynr <ankitbkana@outlook.com>
2024-10-08 00:10:12 -0700

    read

--- app/home-three/team-area.jsx
+++ app/home-three/team-area.jsx
@@ -10,8 +10,8 @@ const Teamarea = () => {
         <div className="row mb-30">
           <div className="col-xl-12">
             <div className="team__area-title">
-              <span className="subtitle__one">Our Exprt Meamber</span>
-              <h2>Expert Our Services man</h2>
+              <span className="subtitle__one">Student Corner</span>
+              <h2>Facilities @ CBS</h2>
             </div>
           </div>
         </div>
@@ -20,14 +20,13 @@ const Teamarea = () => {
             <div className="col-xl-3 col-lg-4 col-md-6 mt-30" key={id}>
               <div className="team__area-item">
                 <div className="team__area-item-image">
-                  <Image alt="" src={data.img} />
+                  <Image  style={{aspectRatio:8/9,objectFit:'cover'}} className="border border-black rounded" alt="" src={data.img} />
                   <div className="team__area-item-image-overlay">
                     <div className="team__area-item-image-overlay-social">
                       <Social />
                     </div>
-                    <div className="team__area-item-image-overlay-content">
-                      <h5>{data.name}</h5>
-                      <span>{data.status}</span>
+                    <div className="team__area-item-image-overlay-content blur-lg border border-white">
+                        <h5 className="text-white ">{data.name}</h5>
                     </div>
                   </div>
                 </div>
--- components/data/team.js
+++ components/data/team.js
@@ -1,28 +1,29 @@
 import team6 from "@/public/img/team/team-10.jpg";
-import team1 from "@/public/img/team/team-5.jpg";
-import team2 from "@/public/img/team/team-6.jpg";
-import team3 from "@/public/img/team/team-7.jpg";
-import team4 from "@/public/img/team/team-8.jpg";
+import team1 from "@/public/img/team/team-1.jpg";
+import team2 from "@/public/img/team/team-2.jpg";
+import team3 from "@/public/img/team/team-3.jpg";
+import team4 from "@/public/img/team/team-4.jpg";
 import team5 from "@/public/img/team/team-9.jpg";
 
 const teams = [
     {
-        name: "Michelle yeoh",
+        name: "ClassRoom",
         status: "Senior Member",
-        img: team1
+        img: team1 
+        
     },
     {
-        name: "James Damian",
+        name: "Laboratory",
         status: "Senior Member",
         img: team2
     },
     {
-        name: "Davinci Resolve",
+        name: "Computer Lab",
         status: "Senior Member",
         img: team3
     },
     {
-        name: "David Fincher",
+        name: "Sports",
         status: "Senior Member",
         img: team4
     },
